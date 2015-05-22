721793b0c2e2d7472cece77c6b575e395763ab37
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-05-22 13:24:13 +0300

    Removed ::apache2::config::locations
    This define was not very useful nor well documented.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -4,10 +4,5 @@
 # Do basic configuration of Apache2
 #
 class apache2::config {
-
     include ::apache2::params
-
-    # Add file containing LocationMatch rules
-    apache2::config::locations { 'default-locations': }
-
 }
