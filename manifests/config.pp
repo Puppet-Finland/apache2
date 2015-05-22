421fbc4a24fdc93134c84a03b24f951938b3eeb4
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-05-22 13:22:41 +0300

    Removed the static core configuration file
    Having a single core configuration file is not particularly portable nor very
    useful.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,17 +7,6 @@ class apache2::config {
 
     include ::apache2::params
 
-    file { 'apache2-core':
-        ensure  => present,
-        name    => "${::apache2::params::conf_d_dir}/core",
-        content => template('apache2/core.erb'),
-        owner   => root,
-        group   => root,
-        mode    => '0644',
-        require => Class['apache2::install'],
-        notify  => Class['apache2::service'],
-    }
-
     # Add file containing LocationMatch rules
     apache2::config::locations { 'default-locations': }
 
