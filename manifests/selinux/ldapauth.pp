5c864c4f8e35f39ef4368365959bae78914b537c
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-06-26 20:50:41 +0300

    Remove dependency to puppetfinland/selinux
    The selinux::boolean type was useless as a built-in type (selboolean) already
    exists.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/selinux/ldapauth.pp
+++ manifests/selinux/ldapauth.pp
@@ -6,8 +6,9 @@
 class apache2::selinux::ldapauth inherits apache2::params {
 
     if $::selinux {
-        selinux::boolean { $::apache2::params::httpd_can_connect_ldap:
-            ensure => 'enabled',
+        selboolean { $::apache2::params::httpd_can_connect_ldap:
+            value      => on,
+            persistent => true,
         }
     }
 }
