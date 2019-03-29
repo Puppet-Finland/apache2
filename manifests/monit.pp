11efdd284bcbf8f007830947e409d29419921141
Samuli Seppänen <samuli@openvpn.net>
2019-03-29 07:31:02 +0200

    Make monit fragment virtual
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/monit.pp
+++ manifests/monit.pp
@@ -8,8 +8,9 @@ class apache2::monit
     String $monitor_email
 )
 {
-  monit::fragment { 'apache2-apache2.monit':
+  @monit::fragment { 'apache2-apache2.monit':
     basename   => 'apache2',
     modulename => 'apache2',
+    tag        => 'default',
   }
 }
